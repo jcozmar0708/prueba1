3a95ea8b8ab327a94c3654e830df7ff5324a15f3
jcozmar0708 <jcozmar0708@g.educaand.es>
2023-10-04 10:35:41 +0200

    ejercicio1Bien

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,20 +1,19 @@
 import java.util.Scanner;
-
 public class Ejercicio1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Introduce a");
-        int a = sc.nextInt();
+        double a = sc.nextDouble();
         System.out.println("Introduce b");
-        int b = sc.nextInt();
+        double b = sc.nextDouble();
         System.out.println("Introduce c");
-        int c = sc.nextInt();
+        double c = sc.nextDouble();
 
         System.out.print("El resultado del apartado a es: ");
         System.out.println(a*b + c*(3-a)/2*b);
         System.out.print("El resutlado del apartado b es: ");
-        System.out.println(((2+a*b)/4)^(c+2));
+        System.out.println(Math.pow((2+ a*b)/4,c+2));
         System.out.print("El resultado del apartado c es: ");
-        System.out.println(-b + ((b^2 - 4*a*c)^1/2)/(2*a));
+        System.out.println((-b + Math.sqrt(Math.pow(b,2) - 4*a*c))/(2*a));
     }
 }

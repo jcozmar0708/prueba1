894f7b09a20d63f9d5e23eaefc3f0abb0e4e8f0a
jcozmar0708 <jcozmar0708@g.educaand.es>
2023-10-04 10:44:32 +0200

    ejercicio1_2_3Bien

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -4,9 +4,9 @@ public class Ejercicio2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Introduce la temperatura en grados C");
-        int gradosC = sc.nextInt();
+        double gradosC = sc.nextDouble();
         System.out.println("A que lo quieres convertir, Fahrenheit (F) o Kelvin (K)");
         String conversion = sc.next();
-        System.out.println(conversion.equals("F") ? (9/5) * gradosC + 32 : gradosC + 273.15);
+        System.out.println(conversion.equals("F") ? gradosC * 9/5 + 32 : gradosC + 273.15);
     }
 }

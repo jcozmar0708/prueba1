6854dec5826d37eecac5f656cb10ff9b1f72497a
jcozmar0708 <jcozmar0708@g.educaand.es>
2023-10-04 10:21:33 +0200

    version inicial

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -6,5 +6,7 @@ public class Ejercicio2 {
         System.out.println("Introduce la temperatura en grados C");
         int gradosC = sc.nextInt();
         System.out.println("A que lo quieres convertir, Fahrenheit (F) o Kelvin (K)");
+        String conversion = sc.next();
+        System.out.println(conversion.equals("F") ? (9/5) * gradosC + 32 : gradosC + 273.15);
     }
 }
--- Ejercicio4.java
+++ Ejercicio4.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Ejercicio4 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escribe el primer numero entero");
+        int a = sc.nextInt();
+        System.out.println("Escribe el segundo numero entero");
+        int b = sc.nextInt();
+        System.out.println("Escribe el tercer numero entero");
+        int c = sc.nextInt();
     }
 }
